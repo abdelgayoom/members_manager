ddc278ea167e4379a686b4acf2e19de348804f53
abdelgayoom <abdu86069@gmail.com>
2020-05-24 00:32:52 +0200

    display member

--- database.py
+++ database.py
@@ -5,19 +5,23 @@ class Db:
     def __init__(self, db):
         self.db = sqlite3.connect(db)
         self.cursor = self.db.cursor()
-        command = "CREATE TABLE members (id INTEGER PRIMARY KEY, first_name TEXT, last_name TEXT, email TEXT, " \
+        command = "CREATE TABLE IF NOT EXISTS members (id INTEGER PRIMARY KEY, first_name TEXT, last_name TEXT, " \
+                  "email TEXT, " \
                   "phone INTEGER, home_address TEXT) "
         self.cursor.execute(command)
         self.db.commit()
 
-    def fetch(self):
-        self.cursor.execute('SELECT * FROM members')
+    def fetch(self, first_name, email):
+        command = 'SELECT * FROM members WHERE first_name=? AND email=?'
+        value = (first_name, email)
+        self.cursor.execute(command, value)
         rows = self.cursor.fetchall()
         return rows
 
     def insert(self, first_name, last_name, email, phone, home_address):
-        command = 'INSERT IN TO members VALUES (NULL, ?, ?, ?, ?,?),(first_name,last_name,email,phone,home_address)'
-        self.cursor.execute(command)
+        command = 'INSERT INTO members VALUES (NULL, ?, ?, ?, ?,?)'
+        value = (first_name, last_name, email, phone, home_address)
+        self.cursor.execute(command, value)
         self.db.commit()
 
     def __del__(self):
--- student.py
+++ student.py
@@ -1,12 +1,28 @@
 import tkinter as tk
+from tkinter.constants import *
+from tkinter import messagebox
+from members_manager.database import Db
+
+db = Db('members.db')
 
 
 class Member(tk.Tk):
-    def __init__(self):
+    def __init__(self, member=None):
         super().__init__()
         self.title('Student Manager')
-        self.geometry('500x600')
+        self.geometry('600x600')
+        if not member:
+            self.member = []
+        else:
+            self.member = member
 
+        # member display section
+        self.member_display_frame = tk.Frame(self)
+        self.member_display_frame.grid(row=1, column=0, ipadx=20, ipady=20)
+        self.member_info = tk.Listbox(self.member_display_frame, height=40, width=40)
+        self.member_info.grid(row=0, column=0, rowspan=3, columnspan=3)
+
+        # registering member section
         self.user_enter_frame = tk.Frame(self)
         self.user_enter_frame.grid(row=0, column=0)
 
@@ -43,21 +59,57 @@ class Member(tk.Tk):
         self.add_member_button = tk.Button(self.user_enter_frame, text='Add Member', command=self.add_member)
         self.add_member_button.grid(row=5, column=0)
 
+        # getting member info section
+        self.user_getting_frame = tk.Frame(self)
+        self.user_getting_frame.grid(row=0, column=1)
+
+        self.first_name_g = tk.Label(self.user_getting_frame, text='First Name :', padx=10, pady=10)
+        self.first_name_g.grid(row=0, column=0)
+        self.first_name_g_text = tk.StringVar()
+        self.first_name_g_text = tk.Entry(self.user_getting_frame, textvariable=self.first_name_g_text)
+        self.first_name_g_text.grid(row=0, column=1)
+
+        self.email_g = tk.Label(self.user_getting_frame, text='Email Address:', padx=10, pady=10)
+        self.email_g.grid(row=1, column=0)
+        self.email_g_text = tk.StringVar()
+        self.email_g_text = tk.Entry(self.user_getting_frame, textvariable=self.email_g_text)
+        self.email_g_text.grid(row=1, column=1)
+
+        self.get_member_button = tk.Button(self.user_getting_frame, text='Search Member', command=self.get_member)
+        self.get_member_button.grid(row=2, column=0)
+
     def add_member(self):
-        pass
+        if self.first_name_text.get() == '' or self.last_name_text.get() == '' or self.email_text.get() == '' or self.phone_text.get() == '' or self.home_address_text.get() == '':
+            messagebox.showerror('Empty Field', 'all fields required')
+            return False
+        db.insert(self.first_name_text.get(), self.last_name_text.get(), self.email_text.get(), self.phone_text.get(),
+                  self.home_address_text.get())
+        self.clear_add_member_inputs()
 
-    def display_member(self):
-        pass
+    def get_member(self):
+        if self.first_name_g_text.get() == '' or self.email_g_text.get() == '':
+            messagebox.showerror('Empty Field', 'all fields required')
+            self.clear_member_search_inputs()
+            return False
+        else:
+            for row in db.fetch(self.first_name_g_text.get(), self.email_g_text.get()):
+                row_dict = list(row)
+                for r in row_dict:
+                    self.member_info.insert(END, r)
+                    self.clear_member_search_inputs()
+
+    def clear_member_search_inputs(self):
+        self.first_name_g_text.delete(0, END)
+        self.email_g_text.delete(0, END)
+
+    def clear_add_member_inputs(self):
+        self.first_name_text.delete(0, END)
+        self.last_name_text.delete(0, END)
+        self.email_text.delete(0, END)
+        self.phone_text.delete(0, END)
+        self.home_address_text.delete(0, END)
 
 
-
-
-
-
-
-
-
-
-if __name__=='__main__':
+if __name__ == '__main__':
     app = Member()
     app.mainloop()
